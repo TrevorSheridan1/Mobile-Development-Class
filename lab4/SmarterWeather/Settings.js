8b01b66a85629e69b829240975c900ca732db0fc
Trevor Sheridan <trevorsheridan@knights.ucf.edu>
2019-04-18 11:29:26 -0400

    fixing folder paths

--- lab4/SmarterWeather/Settings.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-
-// add this to weather project
-<TouchableOpacity style={styles.buttonStyle} onPress={() => navigate('Settings')}>
-    <Text style={styles.buttonText}>Settings</Text>
-</TouchableOpacity>
-
-export default Settings;
